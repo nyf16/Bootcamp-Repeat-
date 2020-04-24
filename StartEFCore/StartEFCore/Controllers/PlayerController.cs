8da1890fb53ee7dfb3d90ad8ff778b4759dc55fa
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-04-25 00:58:41 +0300

    added connection string to appsettings.json

---
+++ StartEFCore/StartEFCore/Controllers/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartEFCore.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // TeamId değerine eşit gelecek id parametresi alır
+        // TODO: Takımın oyuncularını listele (List)
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+
+        //TODO: Yeni oyuncu oluşturmak belirtilen takım için (Create)
+
+        //TODO: Id'si eşit olan oyuncunun bilgileri (Detail)
+
+        //TODO: Id'si eşit olan oyuncunun bilgilerini güncelle (Update)
+
+        //TODO: Id'si eşit olan oyuncuyu sil (Delete)
+    }
+}
---
+++ StartEFCore/StartEFCore/Controllers/TeamController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartEFCore.Controllers
+{
+    public class TeamController : Controller
+    {
+        // TODO: Takımları listelemek (List)
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //TODO: Yeni takım oluşturmak (Create)
+
+        //TODO: Id'si eşit olan takımın bilgileri (Detail)
+
+        //TODO: Id'si eşit olan takımın bilgilerini güncelle (Update)
+
+        //TODO: Id'si eşit olan takımı sil (Delete)
+
+    }
+}
