bf57492797ff495df8570501348db168dafc248f
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-05-06 05:10:03 +0300

    entitybase class created

---
+++ StartEFCore/StartEFCore/Models/Shared/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartEFCore.Models.Shared
+{
+    // Generic Base Class
+    public class Entity<T> : CommonEntity
+    {
+        // [Key]
+        public T Id { get; set; }
+        // Generic olarak belirtmek onun tip değişkenliğine sahip olması demek
+    }
+    // Default Base Class
+    public class Entity : CommonEntity
+    {
+        // [Key]
+        public int Id { get; set; }
+    }
+    // Non Generic Base Class Field
+    public class CommonEntity
+    {
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? ModifiedDate { get; set; }
+        public string HiddenValue { get; set; }
+    }
+}
