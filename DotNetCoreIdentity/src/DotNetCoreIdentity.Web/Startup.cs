45863e8503e7e427290ef3fdf77f0ecfd1ffae43
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-05-16 02:49:33 +0300

    EF configurations started

--- DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Startup.cs
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreIdentity.EF.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,18 @@ namespace DotNetCoreIdentity.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // veritabaný adresini ef'e gönderme ve onu servislere ekleme
+
+            services.AddDbContext<ApplicationUserDbContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DotNetCoreIdentityDb")
+                ));
+
+
+
+
+
+
             services.AddControllersWithViews();
         }
 
