8106515bc4316d913fda42b5dbf5d7de623cbfd2
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-05-19 03:54:52 +0300

    Register function completed

--- DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Controllers/AccountController.cs
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Controllers/AccountController.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreIdentity.Domain.Identity;
 using DotNetCoreIdentity.Web.ViewModels.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCoreIdentity.Web.Controllers
 {
     public class AccountController : Controller
     {
+        // Kullanici kaydetmek için veya kullanici bilgilerinde değişiklik yapmak için kullanılan servis.
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        // Kullanicinin uygulamaya giriş - çıkış işlemlerini yönettiğimiz servis.
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -25,9 +40,44 @@ namespace DotNetCoreIdentity.Web.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            return View();
+            // gelen modeli valide et
+            if (ModelState.IsValid)
+            {
+                // Validse kaydet
+
+                // ApplicationUser Oluştur
+                var newUser = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    //FirstName = model.FirstName,
+                    //LastName = model.LastName,
+                    EmailConfirmed = true,
+                    TwoFactorEnabled = false,
+                    NationalIdNumber = model.NationalIdNumber
+                };
+
+                var registerUser = await _userManager.CreateAsync(newUser, model.Password);
+                if (registerUser.Succeeded)
+                {
+                    await _signInManager.SignInAsync(newUser, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                // Kaydetme başarısızsa hatalari modelstate'e ekle
+                AddErrors(registerUser);
+            }
+            // Değilse hatalari dön
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var err in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, err.Description);
+            }
         }
     }
 }
