27f998a4c6f08e92138e4dc8b47bf50ed53afc16
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-08-22 18:06:48 +0300

    UrlName migration added

--- DotNetCoreIdentity/src/DotNetCoreIdentity.EF/Migrations/20200822144523_urlname.Designer.cs
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.EF/Migrations/20200822144523_urlname.Designer.cs
@@ -4,14 +4,16 @@ using DotNetCoreIdentity.EF.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DotNetCoreIdentity.EF.Migrations
 {
     [DbContext(typeof(ApplicationUserDbContext))]
-    partial class ApplicationUserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200822144523_urlname")]
+    partial class urlname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.EF/Migrations/20200822144523_urlname.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetCoreIdentity.EF.Migrations
+{
+    public partial class urlname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UrlName",
+                table: "Posts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlName",
+                table: "Posts");
+        }
+    }
+}
